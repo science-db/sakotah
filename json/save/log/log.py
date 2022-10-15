a25113dae87d95709917b2d6d4ecbf4bf67099e5
50cK3t5 <98499433+science-db@users.noreply.github.com>
2022-10-15 15:02:03 -0300

    Create log.py

---
+++ json/save/log/log.py
@@ -0,0 +1,13 @@
+!/user/bin/env python3
+import subprocess
+import platform
+import socket
+import time
+import os
+import py_compile
+import PyQt5.QtWidgets
+import PyQt5.QtGui
+import PyQt5.QtCore
+import PyQt5.QtWebEngineWidgets
+import testfactor
+print("log:" + host_ip)
